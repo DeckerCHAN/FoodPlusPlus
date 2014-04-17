87c3cb9f8fcb07ae787dc3f51d252f78d976e2e1
DeckerCHAN <DeckerCHAN@gmail.com>
2014-04-17 23:31:50 +0800

    Change group title

---
+++ src/main/java/com/bukkit/plugin/java/Component/ConfigManager.java
@@ -0,0 +1,55 @@
+package com.bukkit.plugin.java.Component;
+
+import java.io.File;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.Plugin;
+
+public class ConfigManager
+{
+    Plugin ProvidePlugin;
+    FileConfiguration Config;
+
+    private ConfigManager(Plugin plugin)
+    {
+	this.ProvidePlugin = plugin;
+	plugin.getConfig();
+    }
+
+    private static ConfigManager _Instance;
+
+    public static ConfigManager Init(Plugin plugin)
+    {
+	//Check File Exists
+	if(!new File(plugin.getDataFolder() , "config.yml").exists())
+	{
+	    plugin.saveDefaultConfig();
+	}
+	if(!new File(plugin.getDataFolder() , "Exceptions").exists())
+	{
+	    new File(plugin.getDataFolder() , "Exceptions").mkdir();
+	}
+	if (_Instance == null)
+	{
+	    _Instance = new ConfigManager(plugin);
+	}
+	return _Instance;
+    }
+
+    public static FileConfiguration GetConfig()
+    {
+	return _Instance.Config;
+    }
+    /**
+     * Reload configuration to instance
+     */
+    public static void ReloadConfig()
+    {
+	_Instance.ProvidePlugin.reloadConfig();
+	_Instance.Config=_Instance.ProvidePlugin.getConfig();
+    }
+    {
+	
+    }
+
+}
---
+++ src/main/java/com/bukkit/plugin/java/Component/PluginLogger.java
@@ -0,0 +1,50 @@
+package com.bukkit.plugin.java.Component;
+
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.Plugin;
+
+/**
+ * Provide multi type log function
+ * @author Decker
+ * 
+ */
+public class PluginLogger
+{
+    Plugin ProvidePlugin;
+    Logger PluginLogger;
+    private PluginLogger(Plugin plugin)
+    {
+	this.ProvidePlugin = plugin;
+	this.PluginLogger = this.ProvidePlugin.getLogger();
+    }
+    
+    
+    
+    private static PluginLogger _Instance;
+    
+    public static PluginLogger Init(Plugin plugin)
+    {
+	if(_Instance==null)
+	{
+	    _Instance =new PluginLogger(plugin);
+	}
+	return _Instance;
+    }
+    
+
+    public static void Info(String messege)
+    {
+	_Instance.PluginLogger.info(messege);
+    }
+    
+    public static void Warning(String messege)
+    {
+	_Instance.PluginLogger.warning(messege);
+    }
+    
+    public static void Warning(Exception exception)
+    {
+	_Instance.PluginLogger.warning(exception.toString());
+    }
+}
